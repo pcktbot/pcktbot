4df87fde63774955543dd06c8b286aa3b43ad429
pcktbot <pcktbot@gmail.com>
2026-01-11 11:17:02 -0600

    i hate all this

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,12 @@
 <script setup lang="ts">
-import HeaderControls from './components/HeaderControls.vue';
-import ComponentWrapper from './components/ComponentWrapper.vue';
 
 </script>
 
 <template>
-  <header class="header flex justify-between items-center p-4 bg-red-700 text-white">
-    <h1 class="ibm-plex-mono-semibold">pcktbot</h1>
-    <HeaderControls />
-  </header>
+
 
   <main>
     <div class="component-container">
-      <ComponentWrapper id="wrapper-1" />
-      <ComponentWrapper id="wrapper-2" />
-      <ComponentWrapper id="wrapper-3" />
-      <ComponentWrapper id="wrapper-4" />
     </div>
   </main>
 </template>
