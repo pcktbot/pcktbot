90c3baeea73c1d6e9424012c281d7d0fb1eadaee
pcktbot <pcktbot@gmail.com>
2026-01-24 09:12:46 -0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+
+
+  <main>
+    <div class="component-container">
+    </div>
+  </main>
+</template>
+
+<style>
+.component-container {
+  position: relative;
+  width: 100%;
+  height: calc(100vh - 72px); /* Adjust based on your header height */
+  overflow: hidden;
+}
+</style>
